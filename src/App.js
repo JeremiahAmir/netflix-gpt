ade985a9634d285225e34e0d1a21f7fbec44aeda
JeremiahAmir <jeremiah@creative-dots.com>
2024-04-26 01:06:17 +0500

    Create react app and configured Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1 className="text-3xl font-bold text-center mt-5">Netflix GPT</h1>
+        </div>
+    );
+}
+
+export default App;
